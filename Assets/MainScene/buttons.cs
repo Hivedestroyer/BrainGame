6dafd77c6f0a826f970573f689a44afdfd05eb51
Jack Ng <845870934@christchurch.natcoll.ac.nz>
2020-02-01 13:10:49 +1300

    Title scene buttons implemented

---
+++ Assets/MainScene/buttons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void startButton()
+    //to be used to change scene on start button press
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+
+    public void exitButton()
+     //to be used to exit game on exit button press
+    {
+        Application.Quit();
+    }
+}
